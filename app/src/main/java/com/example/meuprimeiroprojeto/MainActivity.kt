482ead37e0a8a5b11d16f6faa8388713ab017ade
Lucas Novaes <luc.novaesm@gmail.com>
2024-05-21 22:54:36 -0300

    Primeiro exercício realizado

---
+++ app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.meuprimeiroprojeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortear(view: View){
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = java.util.Random().nextInt(11)
+        textoResultado.setText("número gerado $numero")
+    }
+
+}
